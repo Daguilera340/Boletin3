4a9db019171129359fcd828a09226180029ff00b
daguilera <daguilera@INF1-PC01>
2017-03-29 20:02:54 +0200

    Commit INDEX

--- Boletin3/index.php
+++ Boletin3/index.php
@@ -1,17 +1,110 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+
+ <head>
+  <meta charset="UTF-8">
+  <title>Índice</title>
+ 
+  <style>
+  
+  	body{
+  	
+  		background-color:#C0C0C0;
+  	
+  	}
+  
+  	div{
+  		border:1px solid black;
+  		border-collapse:collapse;	
+  		text-align:center;
+  		float:left;
+  		margin: 0px 5px;
+  		width:25%;
+  		background-color:#708090;
+  	
+  	}
+  	
+  	a{
+  		display:inline-block;
+  		text-decoration:none;
+  		color:white;
+  		background-color:#4682B4;
+  		width:100%;
+  		border-bottom:1px solid black;
+  		
+  	
+  	}
+  	
+  	a:hover{
+  	
+  		color:black;
+  		background-color:#e7e7e7;
+  	
+  	}
+
+  	a:visited{
+
+		background-color:#008080;
+
+  	}
+  	a:visited:hover{
+  	
+  		background-color:#e7e7e7;
+  	
+  	}
+  
+  </style>
+ 
+ </head>
+ <body>
+ 
+  
+  
+  <h2>BOLETIN 3</h2>
+
+  <a href="1.php" target="_blank">Ejercicio 2</a>
+  <br>
+  <a href="2.php" target="_blank">Ejercicio 3</a>
+  <br>
+  <a href="3.php" target="_blank">Ejercicio 4</a>
+  <br>
+  <a href="4.php" target="_blank">Ejercicio 5</a>
+  <br>
+  <a href="5.php" target="_blank">Ejercicio 6</a>
+  <br>
+  <a href="6.php" target="_blank">Ejercicio 7</a>
+  <br>
+  <a href="7.php" target="_blank">Ejercicio 8</a>
+  <br>
+  <a href="8.php" target="_blank">Ejercicio 9</a>
+  <br>
+  <a href="9.php" target="_blank">Ejercicio 10</a>
+  <br>
+  <a href="10" target="_blank">Ejercicio 11</a>
+  <br>
+  <a href="11.php" target="_blank">Ejercicio 12</a>
+  <br>
+  <a href="12.php" target="_blank">Ejercicio 13</a>
+  <br>
+  <a href="13.php" target="_blank">Ejercicio 14</a>
+  <br>
+  <a href="14.php" target="_blank">Ejercicio 15</a>
+  <br>
+  <a href="15.php" target="_blank">Ejercicio 16</a>
+  <br>
+  <a href="16.php" target="_blank">Ejercicio 17</a>
+  <br>
+  <a href="17.php" target="_blank">Ejercicio 18</a>
+  <br>
+  <a href="18.php" target="_blank">Ejercicio 19</a>
+  <br>
+  <a href="19.php" target="_blank">Ejercicio 20</a>
+  <br>
+  <a href="20.php" target="_blank">Ejercicio 21</a>
+  <br>
+  <a href="21.php" target="_blank">Ejercicio 22</a>
+  
+  </div>
+ 
+ </body>
+
 </html>
